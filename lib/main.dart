cdb9ae558ac1a5e49bf06bdb388098ae1cc30f21
keinuma <nununu.mono@gmail.com>
2020-11-15 16:55:45 +0900

    feat: add stateful widget have stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -37,9 +37,72 @@ class _ParentWidgetState extends State<ParentWidget> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: TapboxB(
+      child: TapboxC(
         active: _active,
         onChanged: _handleTapboxChanged,
+      ),
+    );
+  }
+}
+
+class TapboxC extends StatefulWidget {
+  TapboxC({Key key, this.active: false, @required this.onChanged})
+    : super(key: key);
+  
+  final bool active;
+  final ValueChanged<bool> onChanged;
+
+  _TapboxCState createState() => _TapboxCState();
+}
+
+class _TapboxCState extends State<TapboxC> {
+  bool _heighlight = false;
+
+  void _handleTapDown(TapDownDetails details) {
+    setState(() {
+      _heighlight = true;
+    });
+  }
+
+  void _handleTapUp(TapUpDetails details) {
+    setState(() {
+      _heighlight = false;
+    });
+  }
+
+  void _handleTapCancel() {
+    setState(() {
+      _heighlight = false;
+    });
+  }
+
+  void _handleTap() {
+    widget.onChanged(!widget.active);
+  }
+
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTapDown: _handleTapDown,
+      onTapUp: _handleTapUp,
+      onTapCancel: _handleTapCancel,
+      onTap: _handleTap,
+      child: Container(
+        child: Center(
+          child: Text(widget.active ? 'Active' : 'Inactive',
+            style: TextStyle(fontSize: 32.0, color: Colors.white),)
+        ),
+        width: 200.0,
+        height: 200.0,
+        decoration: BoxDecoration(
+          color:
+            widget.active ? Colors.red[700] : Colors.grey[600],
+          border: _heighlight
+            ? Border.all(
+              color: Colors.teal[700],
+              width: 1.0,
+            )
+            : null,
+        ),
       ),
     );
   }
